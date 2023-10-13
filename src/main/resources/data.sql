a4bd60046e449d743b60830eb1d40578be16f23a
Sam1549 <Sam1549@yandex.ru>
2023-10-13 13:50:27 +0400

    add liquibase

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-insert into person (name, surname,age,phone_of_number,city_of_living) values ('Ivan','Morozov',25,'+79119129191','Moscow');
-insert into person (name,surname,age,phone_of_number,city_of_living) values ('Vova','Ivanov',28,'+79129159560','Tver');
-insert into person (name,surname,age,phone_of_number,city_of_living) values ('Vlad','Petrov',31,'+79149189392','Samara');
-insert into person (name,surname,age,phone_of_number,city_of_living) values ('Kirill','Semenov',23,'+78199219897','Rostov');
-insert into person (name,surname,age,phone_of_number,city_of_living) values ('Pavel','Smirnov',29,'+79109209091','Tomsk');
