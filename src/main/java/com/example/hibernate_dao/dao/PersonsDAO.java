97b720c1131b0218e83bec696b813bc40ee10947
Sam1549 <Sam1549@yandex.ru>
2023-10-17 19:43:29 +0400

    add query

--- src/main/java/com/example/hibernate_dao/dao/PersonsDAO.java
+++ src/main/java/com/example/hibernate_dao/dao/PersonsDAO.java
@@ -3,6 +3,8 @@ package com.example.hibernate_dao.dao;
 import com.example.hibernate_dao.models.Person;
 import com.example.hibernate_dao.models.PersonalData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 
 import java.util.List;
@@ -10,10 +12,13 @@ import java.util.Optional;
 
 
 public interface PersonsDAO extends JpaRepository<Person, PersonalData> {
-    List<Person> findPersonByCityOfLiving(String city);
+    @Query("select p from Person p where p.cityOfLiving =:city")
+    List<Person> findPersonByCityOfLiving(@Param("city") String city);
 
-    List<Person> findPersonByPersonalDataAgeBeforeOrderByPersonalDataAge(int age);
+    @Query("select p from Person p where p.personalData.age < :age order by p.personalData.age")
+    List<Person> findPersonByPersonalDataAgeBeforeOrderByPersonalDataAge(@Param("age") int age);
 
-    List<Optional<Person>> findPersonByPersonalData_NameAndPersonalData_Surname(String name, String surname);
+    @Query("select p from Person p where p.personalData.name = :name and p.personalData.surname = :surname")
+    List<Optional<Person>> findPersonByPersonalData_NameAndPersonalData_Surname(@Param("name") String name, @Param("surname") String surname);
 
 }
